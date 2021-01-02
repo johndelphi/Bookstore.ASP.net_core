c5a6a8876c9789afb1f4bb822eccbe942cd975a1
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-02 00:46:15 -0600

    made method to get al books

---
+++ Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+  public class BookController : Controller
+  {
+   public string GetBook()
+    {
+      return "Book";
+    }
+    
+  }
+}

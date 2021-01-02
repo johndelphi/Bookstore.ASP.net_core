0868810458004aaad10525f34bf2ab87cbf9b525
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-02 13:27:07 -0600

    changed the search method to use contains method

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -17,16 +17,17 @@ namespace Bookstore.Repository
       return DataSource().Where(X => X.Id == id).FirstOrDefault();
 
     }
-    public List<Books>SearchBook(string Title,string authorName)
+    public List<Books>SearchBook(string title,string authorName)
     {
-
+      return DataSource().Where(x => x.Title.Contains (title) && x.Author.Contains(authorName)).ToList();
     }
     public List<Books> DataSource()
     {
       return new List<Books>()
       {
         new Books(){Id=1,Title="phyton",Author="teric"},
-        new Books(){Id=2,Title="macs",Author="longhorn"}
+        new Books(){Id=2,Title="macs",Author="longhorn"},
+        new Books(){Id=3,Title="culculas",Author="thomson"}
       };
     }
   }

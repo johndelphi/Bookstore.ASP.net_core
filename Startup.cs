b518596f0848bf45e0dbf1c845e8066e6ded6bf2
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-01 23:26:35 -0600

    changed routing fomat

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,10 @@ namespace Bookstore
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddControllersWithViews();
+      
     }
+    
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,17 +29,15 @@ namespace Bookstore
         app.UseDeveloperExceptionPage();
       }
 
-      app.Use(async (context, next) => { await context.Response.WriteAsync("hey just made a middleware"); }
-     );
-      //app.UseRouting();
+      
+      app.UseRouting();
 
-      //app.UseEndpoints(endpoints =>
-      //{
-      //  endpoints.MapGet("/", async context =>
-      //        {
-      //      await context.Response.WriteAsync("Hello World!");
-      //    });
-      //});
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapDefaultControllerRoute();
+              
+      });
+      
     }
   }
 }

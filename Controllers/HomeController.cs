5fcc96ee428cdd1da1f063a9e21478cc84af3956
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-02 12:54:44 -0600

    Revert "added index method"
    This reverts commit dc24a21bb8e0e1e795a7bea831803625aa0e7ce9.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Bookstore.Controllers
 {
   public class HomeController : Controller
   {
-    public string Index()
+    public string GetAllBooks()
     {
       return "all books";
     }
